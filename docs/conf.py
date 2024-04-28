aa9fd9ab36308a62b9a1495de5360bcfe39bdc8b
OpenSource Engineer <37376566+castetsb@users.noreply.github.com>
2024-04-28 13:43:50 -0400

    Settings for read the doc

--- docs/conf.py
+++ docs/conf.py
@@ -8,7 +8,15 @@
 
 import os
 import sys
-sys.path.insert(0,os.path.abspath(".."))
+
+# Get the project root dir, which is the parent dir of this
+cwd = os.getcwd()
+project_root = os.path.dirname(cwd)
+
+# Insert the project root dir as the first element in the PYTHONPATH.
+# This lets us ensure that the source package is imported, and that its
+# version is used.
+sys.path.insert(0, project_root)
 
 project = 'pyRobotiqGripper'
 copyright = '2024, Benoit CASTETS'
@@ -18,7 +26,12 @@ release = '1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["sphinx.ext.todo","sphinx.ext.viewcode","sphinx.ext.autodoc","sphinx_rtd_theme"]
+extensions = [
+    "sphinx.ext.todo",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.duration"]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
