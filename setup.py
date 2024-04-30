4e0c31594e18ba9303e31547ba693073f5b6c33e
OpenSource Engineer <37376566+castetsb@users.noreply.github.com>
2024-04-30 00:08:26 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='1.0.0',
     packages=find_packages(),
     install_requires=[
-        minimalmodbus
+        'minimalmodbus'
     ],
     author='Benoit CASTETS',
     author_email='opensourceeng@email.com',
