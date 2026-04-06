08b3b9a81493ddcc0c1ab39d8df1d705940614c2
OpenSource Engineer <37376566+castetsb@users.noreply.github.com>
2026-04-05 23:35:27 -0400

    3.1.9

--- pyrobotiqgripper/__init__.py
+++ pyrobotiqgripper/__init__.py
@@ -15,7 +15,7 @@ __author__ = "Benoit CASTETS"
 __email__ = "opensourceeng@robotiq.com"
 __license__ = "Apache License, Version 2.0"
 __url__ = "https://github.com/castetsb/pyRobotiqGripper"
-__version__ = "3.1.8"
+__version__ = "3.1.9"
 __project__ = 'pyrobotiqgripper'
 
 # Main class
